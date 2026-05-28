a9239336c900d8eefb9dd5c1bf4ad28ed0da9e1f
ALLALEN Mohamed Nour <mohamed.allalen@etu.unistra.fr>
2026-05-28 17:14:53 +0200

    :sparkles: feat: Création de LivreController et l'injection de LivreRepository

---
+++ src/main/java/com/portfolio/mediatheque/controller/LivreController.java
@@ -0,0 +1,27 @@
+package com.portfolio.mediatheque.controller;
+
+import com.portfolio.mediatheque.repository.LivreRepository;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+/**
+ * @RestController cette notation dit à SPRING 
+ * cette classe va recevoir des requetes webet tout 
+ * ce qu'elle renvoie doit etre automatiquement tranformé
+ * en texte JSON.
+ * @RequestMapping ("api/livres") définit la route de base
+ */
+
+
+@RestController
+@RequestMapping("/api/livres")
+public class LivreController {
+
+    private final LivreRepository livreRepository;
+
+    // C'est l'injection de dépendance !
+    public LivreController(LivreRepository livreRepository) {
+        this.livreRepository = livreRepository;
+    }
+}
