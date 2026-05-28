913168d1ee603eb042b27ec85a4bffd3399c96de
ALLALEN Mohamed Nour <mohamed.allalen@etu.unistra.fr>
2026-05-28 17:21:46 +0200

    :sparkles: feat: Ajout de la route GET /api/livres pour récupérer les livres

--- src/main/java/com/portfolio/mediatheque/controller/LivreController.java
+++ src/main/java/com/portfolio/mediatheque/controller/LivreController.java
@@ -3,7 +3,9 @@ package com.portfolio.mediatheque.controller;
 import com.portfolio.mediatheque.repository.LivreRepository;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.GetMapping;
+import java.util.List;
+import com.portfolio.mediatheque.model.Livre;
 
 /**
  * @RestController cette notation dit à SPRING 
@@ -24,4 +26,17 @@ public class LivreController {
     public LivreController(LivreRepository livreRepository) {
         this.livreRepository = livreRepository;
     }
+
+    /**
+     * @GetMapping Indique que si on reçoit une requete HTTP de type 
+     * GET sur l'URL de base (/api/livres), c'est cette méthode qui doit s'exécuter.
+     * @findAll La méthode magique 
+     * @return la liste d'objets Java
+     */
+    @GetMapping
+    public List<Livre> getAllLivres(){
+        return livreRepository.findAll();
+    }
 }
+
+
