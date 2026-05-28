6f3d891936727e54beeec15d1e30692be5357431
ALLALEN Mohamed Nour <mohamed.allalen@etu.unistra.fr>
2026-05-28 10:42:06 +0200

    chore: initialisation du projet et configuration de base
    - Création du squelette Spring Boot avec Maven
    - Ajout des dépendances SQLite et Hibernate dans le pom.xml
    - Configuration de la base de données dans application.properties
    - Création du PingController pour vérifier l'état du serveur

---
+++ src/main/java/com/portfolio/mediatheque/Application.java
@@ -0,0 +1,13 @@
+package com.portfolio.mediatheque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    // Vérifie bien cette ligne exacte :
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
