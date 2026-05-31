a0aad17cab924cc43fbd1e2e89765dbac0b79cd5
ALLALEN Mohamed Nour <mohamed.allalen@etu.unistra.fr>
2026-05-31 22:22:48 +0200

    :tada: feat: Ajout de la méthode existsByLivreIdAndDateRetourIsNull pour gérer les emprunts

--- src/main/java/com/portfolio/mediatheque/repository/EmpruntRepository.java
+++ src/main/java/com/portfolio/mediatheque/repository/EmpruntRepository.java
@@ -6,4 +6,18 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface EmpruntRepository extends JpaRepository<Emprunt, Long> {
+    
+    // La méthode magique générée par Spring !
+    // Dis-moi par un simple Oui ou Non (true/false) si le livre numéro X est
+    // actuellement en dehors de la médiathèque.
+    /**
+         * SELECT EXISTS (
+            SELECT 1 
+            FROM emprunt 
+            WHERE livre_id = 1 
+            AND date_retour IS NULL
+            );
+     */
+    boolean existsByLivreIdAndDateRetourIsNull(Long livreId);
+    
 }
