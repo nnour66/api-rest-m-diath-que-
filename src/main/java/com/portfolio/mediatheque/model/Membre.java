8a22a22774781ef2a80d96faf754b93d19bf2c5c
ALLALEN Mohamed Nour <mohamed.allalen@etu.unistra.fr>
2026-05-31 20:02:15 +0200

    :tada: feat: Création de l'entité Menmbre avec un validation des données

---
+++ src/main/java/com/portfolio/mediatheque/model/Membre.java
@@ -0,0 +1,55 @@
+package com.portfolio.mediatheque.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+
+@Entity
+public class Membre {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Le nom est obligatoire")
+    private String nom;
+
+    @NotBlank(message = "Le prénom est obligatoire")
+    private String prenom;
+
+    @NotBlank(message = "L'email est obligatoire")
+    @Email(message = "Format d'email invalide")
+    private String email;
+
+    public Membre(){}
+
+    public Long getId() {
+        return id;
+    }
+    public String getNom() {
+        return nom;
+    }
+    public String getPrenom() {
+        return prenom;
+    }
+    public String getEmail() {
+        return email;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
